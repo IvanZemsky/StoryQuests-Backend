b8600383275dba0f1feda6e1dd55812c09f1ed0e
IvanZemsky <i.ivanzemsky2005@gmail.com>
2025-08-10 23:42:21 +0300

    chore: return empty array when couldn't find stories

--- internal/service/story.go
+++ internal/service/story.go
@@ -13,13 +13,20 @@ func NewStoryService(repo domain.StoryRepository) domain.StoryService {
 }
 
 func (service *storyService) Find(filters domain.StoryFilters) ([]domain.Story, error) {
-	return service.repo.Find(filters)
+	stories, err := service.repo.Find(filters)
+	if err != nil {
+		return nil, err
+	}
+	if len(stories) == 0 {
+		return []domain.Story{}, nil
+	}
+	return stories, nil
 }
 
 func (service *storyService) FindByID(id string) (domain.Story, error) {
 	objID, err := ParseObjectID(id)
 	if err != nil {
-        return domain.Story{}, err
-    }
+		return domain.Story{}, err
+	}
 	return service.repo.FindByID(objID)
 }
